c13f64ca346c5a341db071555980f73af440cac5
ramieres <urami3res@gmail.com>
2018-07-04 21:29:54 +0300

    Lesson 26

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
 
 
 def signup_view(request):
     if request.method == 'POST':
-         form = UserCreationForm(request.POST)
-         if form.is_valid():
-             user = form.save()
-             #  log the user in
-             login(request, user)
-             return redirect('articles:list')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            #  log the user in
+            login(request, user)
+            return redirect('articles:list')
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/signup.html', { 'form': form })
+    return render(request, 'accounts/signup.html', {'form': form})
 
 
 def login_view(request):
@@ -23,10 +24,14 @@ def login_view(request):
             # log the user in
             user = form.get_user()
             login(request, user)
-            return redirect('articles:list')
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+                #return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/login.html', { 'form': form })
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout_view(request):
